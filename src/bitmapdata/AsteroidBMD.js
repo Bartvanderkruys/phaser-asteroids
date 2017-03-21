08bd9769e2de1312fea0963dc58082eddacf6796
Bart van der Kruys <bartvanderkruys@gmail.com>
2017-03-21 20:24:59 +0100

    make asteroids more asteroidy.

--- src/bitmapdata/AsteroidBMD.js
+++ src/bitmapdata/AsteroidBMD.js
@@ -1,11 +1,5 @@
 export const verySmallAsteroidBMD = (game) => {
-    let bmd = game.add.bitmapData(16, 16);
-
-    bmd.ctx.beginPath();
-    bmd.ctx.lineTo(16, 8);
-    bmd.ctx.lineTo(0, 16);
-    bmd.ctx.lineTo(0, 0);
-    bmd.ctx.closePath();
+    let bmd = createAsteroidShape(game, 32);
     bmd.ctx.fillStyle = '#db635a';
     bmd.ctx.fill();
 
@@ -14,13 +8,7 @@ export const verySmallAsteroidBMD = (game) => {
 
 
 export const smallAsteroidBMD = (game) => {
-    let bmd = game.add.bitmapData(32, 32);
-
-    bmd.ctx.beginPath();
-    bmd.ctx.lineTo(32, 16);
-    bmd.ctx.lineTo(0, 32);
-    bmd.ctx.lineTo(0, 0);
-    bmd.ctx.closePath();
+    let bmd = createAsteroidShape(game, 64);
     bmd.ctx.fillStyle = '#d49588';
     bmd.ctx.fill();
 
@@ -28,13 +16,7 @@ export const smallAsteroidBMD = (game) => {
 };
 
 export const mediumAsteroidBMD = (game) => {
-    let bmd = game.add.bitmapData(64, 64);
-
-    bmd.ctx.beginPath();
-    bmd.ctx.lineTo(64, 32);
-    bmd.ctx.lineTo(0, 64);
-    bmd.ctx.lineTo(0, 0);
-    bmd.ctx.closePath();
+    let bmd = createAsteroidShape(game, 126);
     bmd.ctx.fillStyle = '#7ca6b1';
     bmd.ctx.fill();
 
@@ -42,16 +24,55 @@ export const mediumAsteroidBMD = (game) => {
 };
 
 export const largeAsteroidBMD = (game) => {
-    let bmd = game.add.bitmapData(126, 126);
-
-    bmd.ctx.beginPath();
-    bmd.ctx.lineTo(126, 64);
-    bmd.ctx.lineTo(0, 126);
-    bmd.ctx.lineTo(0, 0);
-    bmd.ctx.closePath();
+    let bmd = createAsteroidShape(game, 256);
     bmd.ctx.fillStyle = '#436f6e';
     bmd.ctx.fill();
 
     return bmd;
 };
+
+const createAsteroidShape = (game, size) => {
+    let third = size /3;
+    let bmd = game.add.bitmapData(size, size);
+
+    bmd.ctx.beginPath();
+    bmd.ctx.moveTo(getRandomOffset(third), third + getRandomOffset(third));
+
+    if (Math.random() > 0.3) {
+        bmd.ctx.lineTo(0, 2 * third + getRandomOffset(third));
+    }
+
+    if (Math.random() > 0.3) {
+        bmd.ctx.lineTo(third + getRandomOffset(third), size + getRandomOffset(third));
+    }
+
+    if (Math.random() > 0.3) {
+        bmd.ctx.lineTo(2 * third + getRandomOffset(third), size + getRandomOffset(third));
+    }
+
+    if (Math.random() > 0.3) {
+        bmd.ctx.lineTo(size + getRandomOffset(third), 2 * third + getRandomOffset(third));
+    }
+
+    if (Math.random() > 0.3) {
+        bmd.ctx.lineTo(size + getRandomOffset(third), third + getRandomOffset(third));
+    }
+
+    if (Math.random() > 0.3) {
+        bmd.ctx.lineTo(2 * third + getRandomOffset(third), getRandomOffset(third));
+    }
+
+    if (Math.random() > 0.3) {
+        bmd.ctx.lineTo(third + getRandomOffset(third), getRandomOffset(third));
+    }
+
+    bmd.ctx.closePath();
+
+    return bmd;
+};
+
+function getRandomOffset(third) {
+    return Math.random() * third - third / 2;
+};
+
 
